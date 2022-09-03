f85a7cdf74db0732e2cbe864ab32abb6681315d4
Kirill <kiril-karp4@mail.ru>
2022-09-03 20:21:13 +1000

    Task2

--- Program.cs
+++ Program.cs
@@ -2,21 +2,52 @@
 // и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-Console.WriteLine($"\nЗадача 25 Возведение числа A в натуральную степень B");
+/*Console.WriteLine($"\nЗадача 25 Возведение числа A в натуральную степень B");
 
-int Exponentiation(int numberA, int numberB){
-  int result = 1;
-  for(int i=1; i <= numberB; i++){
-    result = result * numberA;
-  }
+int Exponentiation(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
     // int result = Math.Pow(numberA, numberB);
     return result;
 }
 
-  Console.Write("Введите число A: ");
-  int numberA = Convert.ToInt32(Console.ReadLine());
-  Console.Write("Введите число B: ");
-  int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
 
-  int exponentiation = Exponentiation(numberA, numberB);
-  Console.WriteLine("Результат: " + exponentiation);
+int exponentiation = Exponentiation(numberA, numberB);
+Console.WriteLine("Результат: " + exponentiation);
+*/
+
+//Задача 27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine($"\nЗадача 27. Выдаёт сумму цифр в числе");
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+
